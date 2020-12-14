44a7135b938ef68401ad8308ac535ef5472d0956
maitran32 <74993911+maitran32@users.noreply.github.com>
2020-12-14 14:04:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -219,5 +219,10 @@ def update_unstack_barchart(ctg):
 
 server = app.server
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+    
+#if __name__ == '__main__':
+#    app.run_server(debug=True)
