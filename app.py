6348b67c1227e2208320f7126f74d47ec01e6567
hendrasetiawanbentley <74999581+hendrasetiawanbentley@users.noreply.github.com>
2020-12-15 09:51:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -17,8 +17,8 @@ import plotly.express as px
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-server = app.server
+app.layout = html.Div([html.H1('Hello', style={'textAlign': 'center'})])
 
-server = app.server
+    server = app.server
 if __name__ == '__main__':
     app.run_server(debug=True)
