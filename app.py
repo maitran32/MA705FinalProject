0d874df291ca6d7c70e2717dda400cb4d2578095
hendrasetiawanbentley <74999581+hendrasetiawanbentley@users.noreply.github.com>
2020-12-15 09:49:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ Created on Wed Dec  9 15:34:11 2020
 
 """
 
-import pandas as pd 
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas as pd
 import plotly.express as px
 
 
@@ -19,8 +19,6 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 server = app.server
 
-marketingdata = pd.read_csv('marketingdata.csv', usecols=['EMPLOYER', 'STATE', 'JOB TITLE', 'BASE SALARY', 'STATE', 'SUBMIT YEAR', 'START YEAR'])
-#create a histogram for distribution of H1B in all states over the year (submit year
-
+server = app.server
 if __name__ == '__main__':
     app.run_server(debug=True)
