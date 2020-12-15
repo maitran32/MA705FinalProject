5e7bb564f38e16e5db39212256b5f79073908c27
hendrasetiawanbentley <74999581+hendrasetiawanbentley@users.noreply.github.com>
2020-12-15 09:51:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -19,6 +19,6 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([html.H1('Hello', style={'textAlign': 'center'})])
 
-    server = app.server
+server = app.server
 if __name__ == '__main__':
     app.run_server(debug=True)
